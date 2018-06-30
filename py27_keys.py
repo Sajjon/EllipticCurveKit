1d1c800f329fa52887d5c29ef0b75ba108f7722a
Sajjon <alex.cyon@gmail.com>
2018-06-30 22:31:52 +0200

    wip python signing

--- py27_keys.py
+++ py27_keys.py
@@ -312,27 +312,29 @@ class EllipticCurvePoint:
 
         return (R.x[0]-r)%self.n==0
 
-    def SignMessage(self,message,priv):
+    def SignMessage(self, message, private_key_hex_64):
         #Sign a message. The private key is self.d.
-        (d,uncompressed)=self.DFromPriv(priv)
+        uncompressed = True
+        d = self.DFromPrivateKeyHex64(private_key_hex_64)
 
-        z=Byte2Int(Hash(Hash(MsgMagic(message),"SHA256"),"SHA256"))
+        z = Byte2Int(Hash(Hash(MsgMagic(message), "SHA256"), "SHA256"))
         
-        r=0
-        s=0
+        r = 0
+        s = 0
+
         while not r or not s:
             #k=random.randint(1,self.n-1)
-            k=random.SystemRandom().randint(1,self.n-1) # Better random fix
-            R=self*k
+            k = random.SystemRandom().randint(1, self.n-1) # Better random fix
+            R = self * k
             R.Normalize()
-            r=R.x[0]%self.n
-            s=(InvMod(k,self.n)*(z+r*d))%self.n
+            r = R.x[0] % self.n
+            s = (InvMod(k, self.n) * (z + r * d)) % self.n
 
-        val=27
+        val = 27
         if not uncompressed:
             val+=4
 
-        return base64.standard_b64encode(chr(val)+Int2Byte(r,32)+Int2Byte(s,32))
+        return base64.standard_b64encode(chr(val) + Int2Byte(r, 32) + Int2Byte(s, 32))
 
     def IsValid(self,addr):
         adr=b58decode(addr)
@@ -343,40 +345,104 @@ class EllipticCurvePoint:
 
         return cs==verif
 
-    def VerifyMessageFromAddress(self,addr,message,sig):
+    def VerifyMessageFromAddress(self, public_address_base58_uncompressed, message, sig):
         #Check a signature (r,s) for the message m signed by the Bitcoin 
         # address "addr".
 
-        sign=base64.standard_b64decode(sig)
-        (r,s)=(Byte2Int(sign[1:33]),Byte2Int(sign[33:65]))
+        sign = base64.standard_b64decode(sig)
+        (r, s) = (Byte2Int(sign[1:33]), Byte2Int(sign[33:65]))
 
-        z=Byte2Int(Hash(Hash(MsgMagic(message),"SHA256"),"SHA256"))        
+        z = Byte2Int(Hash(Hash(MsgMagic(message), "SHA256"), "SHA256"))        
 
-        val=ord(sign[0])
-        if val<27 or val>=35:
+        val = ord(sign[0])
+        if val < 27 or val >= 35:
             return False
 
-        if val>=31:
+        if val >= 31:
             uncompressed=False
-            val-=4
+            val -= 4
         else:
             uncompressed=True
         
-        x=r
-        y2=(pow(x,3,self.p) + self.a*x + self.b) % self.p
-        y=Cipolla(y2,self.p)
+        x = r
+        y2 = (pow(x, 3, self.p) + self.a*x + self.b) % self.p
+        y = Cipolla(y2, self.p)
 
         for _ in range(2):
-            kG=EllipticCurvePoint([x,y,1],self.a,self.b,self.p,self.n)  
-            mzG=self*((-z)%self.n)
-            Q=(kG*s+mzG)*InvMod(r,self.n)
-            
-            if self.AddressFromPublicKey(Q,uncompressed)==addr:
+            kG = EllipticCurvePoint([x,y,1],self.a,self.b,self.p,self.n)  
+            mzG = self * ((-z) % self.n)
+            Q = (kG * s + mzG) * InvMod(r, self.n)
+
+            # def CalculatePublicKeyCurvePointFromPrivateKeyBytesD(self, d):
+            #     private_key_point_Q = self * d
+            #     return self.CalculatePublicKeyCurvePointFromPrivateKeyPointQ(private_key_point_Q)
+            # def DeriveKeysAndAddressesFromPrivateKeyWifUncompressedOrCompressed(self, priv_wif_uncompressed):
+            #     (private_key_D, _) = self.DFromPrivateKeyWifBase58Encoded(priv_wif_uncompressed)
+            #     return self.DeriveKeysAndAddressesFromD(private_key_D)
+
+            # def DeriveKeysAndAddressesFromD(self, private_key_D):
+            #     private_key_byte_array = to_bytes_32(private_key_D)
+            # return self.DeriveKeysAndAddressesFromPrivateKeyAsHex64Chars(private_key_byte_array)
+
+            pubaddr_base58 = self.CreatePublicAddressBase58UncompressedFromQ(Q, uncompressed)
+            print "pubaddr_base58 GENERATED"
+            debug(pubaddr_base58)
+            print "EXPECTED:"
+            debug(public_address_base58_uncompressed)
+            if pubaddr_base58 == public_address_base58_uncompressed:
+                assert 1 == 2
                 return True
 
-            y=self.p-y
+            y = self.p - y
 
         return False
+
+    def CreatePublicAddressBase58UncompressedFromQ(self, Q, uncompressed):
+        public_keys = self.CreatePublicKeysFromQ(Q)
+
+        public_key_uncompressed = public_keys.hex_130chars
+        public_key_compressed = public_keys.compressed_hex_66chars
+        # PUBLIC ADDRESSES
+        m = hashlib.new('ripemd160')
+        m.update(hashlib.sha256(public_key_uncompressed).digest())
+        ripe = m.digest() # Step 2 & 3
+    
+        m = hashlib.new('ripemd160')
+        m.update(hashlib.sha256(public_key_compressed).digest())
+        ripe_c = m.digest() # Step 2 & 3
+    
+        bitcoin = [b"\x00", b"\x80"]
+        # litecoin = [b"\x30", b"\xb0"]
+        # darkcoin = [b"\x4c", b"\xcc"]
+        cointype = bitcoin
+    
+        extRipe = cointype[0] + ripe # Step 4
+        extRipe_c = cointype[0] + ripe_c # Step 4
+    
+        chksum = hashlib.sha256(hashlib.sha256(extRipe).digest()).digest()[:4] # Step 5-7
+        chksum_c = hashlib.sha256(hashlib.sha256(extRipe_c).digest()).digest()[:4] # Step 5-7
+    
+        pubaddr = extRipe + chksum # Step 8
+        pubaddr_c = extRipe_c + chksum_c # Step 8
+
+        public_address_uncompressed_base58_25to34chars = b58encode(pubaddr) # OLD: str(b58encode(pubaddr), 'utf-8')
+        public_address_compressed_base58_25to34chars = b58encode(pubaddr_c)
+
+        print "START OF PUB ADD CREATION"
+        debug(public_address_uncompressed_base58_25to34chars)
+        debug(public_address_compressed_base58_25to34chars)
+        print "END OF PUB ADD CREATION"
+
+        if uncompressed:
+        	return public_address_uncompressed_base58_25to34chars
+        else:
+        	return public_address_compressed_base58_25to34chars
+
+    def CreatePublicKeysFromQ(self, Q):
+        point = self.CalculatePublicKeyCurvePointFromPrivateKeyPointQ(Q)
+        public_keys_tuple = self.PublicKeysFromPublicKeyCurvePoint(point)
+        return public_keys_tuple
+
     
     def PrivateKeyWIFBase58UncompressedFromD(self, d):
         p=Int2Byte(d,32)
@@ -425,12 +491,15 @@ class EllipticCurvePoint:
         return PublicKeys(public_key_uncompressed_130char_hex_uppercased, public_key_compressed_66char_hex_uppercased)
 
     def DeriveKeysAndAddressesFromPrivateKeyWifUncompressedOrCompressed(self, priv_wif_uncompressed):
-        (private_key_D, compressed_format) = self.DFromPrivateKeyWifBase58Encoded(priv_wif_uncompressed)
+        (private_key_D, _) = self.DFromPrivateKeyWifBase58Encoded(priv_wif_uncompressed)
+        return self.DeriveKeysAndAddressesFromD(private_key_D)
+
+    def DeriveKeysAndAddressesFromD(self, private_key_D):
         private_key_byte_array = to_bytes_32(private_key_D)
         return self.DeriveKeysAndAddressesFromPrivateKeyAsHex64Chars(private_key_byte_array)
 
-
     # Code found here: https://gist.github.com/UdjinM6/07f1feae8b7495c67480
+    # should be updated to use vanilla 
     def DeriveKeysAndAddressesFromPrivateKeyAsHex64Chars(self, private_key_bytes_array):
         public_key_curve_point = self.CalculatePublicKeyCurvePointFromPrivateKeyHex64Chars(private_key_bytes_array)    
         intermediate_pubkeys = self.PublicKeysFromPublicKeyCurvePoint(public_key_curve_point)
@@ -515,7 +584,23 @@ class EllipticCurvePoint:
 
         assert isinstance(f8.public_addresses.zilliqa_public_address, str)
         assert 42 == len(f8.public_addresses.zilliqa_public_address)
+
+        # TEST SIGNING MESSAGE
+        message_to_sign = "This python code is soon gonna be ported to Swift, yay!!!"
+        self.TestSigningMessage(f8.private_keys.hex_64chars, f8.public_addresses.base58_25to34chars, message_to_sign)
+
         return f8
+
+    def TestSigningMessage(self, private_key_64, public_address_base58_uncompressed, message):
+        #Sign a message with the current address
+        signature = self.SignMessage(message, private_key_64)
+        #Verify the message using only the bitcoin adress, the signature and the message.
+        #Not using the public key as it is not needed.
+        print "Signing message with private key:"
+        debug(private_key_64)
+        print "Verifying with pub addr:"
+        debug(public_address_base58_uncompressed)
+        assert self.VerifyMessageFromAddress(public_address_base58_uncompressed, message, signature), "Should verify"
 
     def CalculatePublicKeyCurvePointFromPrivateKeyHex64Chars(self, privkey_hex_64):
         private_key_bytes_D = self.DFromPrivateKeyHex64(privkey_hex_64)

576a04b30afc7f777c453b801477c608aa32d38c
Sajjon <alex.cyon@gmail.com>
2018-07-21 13:15:57 +0200

    adding expected DER formatted signatures in tests for secp256k1 ECDSA tests

--- Tests/CurveTests/Secp256k1/SigningAndVerificationTests/ECDSA/ECDSATests.swift
+++ Tests/CurveTests/Secp256k1/SigningAndVerificationTests/ECDSA/ECDSATests.swift
@@ -19,7 +19,7 @@ func hash256(for message: String) -> Data {
 /// Creds to: https://raw.githubusercontent.com/oleganza/CoreBitcoin/master/CoreBitcoinTestsOSX/BTCKeyTests.swift
 class ECDSATests: XCTestCase {
 
-    private func verifyRFC6979(_ privateKeyHex: String, _ messageToHash: String, _ expectedNonceKHex: String) {//}, _ expectedSignatureHex: String) {
+    private func verifyRFC6979(_ privateKeyHex: String, _ messageToHash: String, _ expectedNonceKHex: String, _ hexForExpectedSignatureOn_DER_format: String) {
         let hash = hash256(for: messageToHash)
 
 
@@ -31,12 +31,14 @@ class ECDSATests: XCTestCase {
         let message = Message(data: hash)
 
         XCTAssertEqual(expectedNonceKHex, k.asHexString(uppercased: false), "Must produce matching k nonce.")
-//        let signatureFromMessage = AnyKeySigner<ECDSA<Secp256k1>>.sign(message, using: keyPair)
-//        XCTAssertEqual(expectedSignatureHex, signatureFromMessage.asHexString(), "Must produce matching signature.")
+        let signatureFromMessage = AnyKeySigner<ECDSA<Secp256k1>>.sign(message, using: keyPair)
+        XCTAssertEqual(hexForExpectedSignatureOn_DER_format, signatureFromMessage.asHexString(), "Must produce matching signature.")
     }
 
 
     // Test vectors: https://github.com/trezor/trezor-crypto/blob/957b8129bded180c8ac3106e61ff79a1a3df8893/tests/test_check.c#L1959-L1965
+    // Signature data from: https://github.com/oleganza/CoreBitcoin/blob/master/CoreBitcoinTestsOSX/BTCKeyTests.swift
+    //
     // more vectors: https://bitcointalk.org/index.php?topic=285142.msg3300992#msg3300992
     // even more vectors (unreliable??):
     // https://bitcointalk.org/index.php?topic=285142.msg3299061#msg3299061
@@ -44,36 +46,55 @@ class ECDSATests: XCTestCase {
 
         continueAfterFailure = false
 
-        verifyRFC6979("C9AFA9D845BA75166B5C215767B1D6934E50C3DB36E89B127B8A622B120F6721", "sample", "a6e3c57dd01abe90086538398355dd4c3b17aa873382b0f24d6129493d8aad60")
+        verifyRFC6979(
+            "C9AFA9D845BA75166B5C215767B1D6934E50C3DB36E89B127B8A622B120F6721",
+            "sample",
+            "a6e3c57dd01abe90086538398355dd4c3b17aa873382b0f24d6129493d8aad60",
+            "30440220432310e32cb80eb6503a26ce83cc165c783b870845fb8aad6d970889fcd7a6c80220530128b6b81c548874a6305d93ed071ca6e05074d85863d4056ce89b02bfab69"
+        )
 
-        verifyRFC6979("CCA9FBCC1B41E5A95D369EAA6DDCFF73B61A4EFAA279CFC6567E8DAA39CBAF50", "sample", "2df40ca70e639d89528a6b670d9d48d9165fdc0febc0974056bdce192b8e16a3")
-        //expectedSignatureHex: "3045022100af340daf02cc15c8d5d08d7735dfe6b98a474ed373bdb5fbecf7571be52b384202205009fb27f37034a9b24b707b7c6b79ca23ddef9e25f7282e8a797efe53a8f124")
+        verifyRFC6979(
+            "CCA9FBCC1B41E5A95D369EAA6DDCFF73B61A4EFAA279CFC6567E8DAA39CBAF50",
+            "sample",
+            "2df40ca70e639d89528a6b670d9d48d9165fdc0febc0974056bdce192b8e16a3",
+            "3045022100af340daf02cc15c8d5d08d7735dfe6b98a474ed373bdb5fbecf7571be52b384202205009fb27f37034a9b24b707b7c6b79ca23ddef9e25f7282e8a797efe53a8f124"
+        )
 
 
-        verifyRFC6979("0000000000000000000000000000000000000000000000000000000000000001",
-        "Satoshi Nakamoto",
-        "8f8a276c19f4149656b280621e358cce24f5f52542772691ee69063b74f15d15")
-        // expectedSignatureHex: "3045022100934b1ea10a4b3c1757e2b0c017d0b6143ce3c9a7e6a4a49860d7a6ab210ee3d802202442ce9d2b916064108014783e923ec36b49743e2ffa1c4496f01a512aafd9e5"
+        verifyRFC6979(
+            "0000000000000000000000000000000000000000000000000000000000000001",
+            "Satoshi Nakamoto",
+            "8f8a276c19f4149656b280621e358cce24f5f52542772691ee69063b74f15d15",
+            "3045022100934b1ea10a4b3c1757e2b0c017d0b6143ce3c9a7e6a4a49860d7a6ab210ee3d802202442ce9d2b916064108014783e923ec36b49743e2ffa1c4496f01a512aafd9e5"
+        )
 
-        verifyRFC6979("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364140",
-        "Satoshi Nakamoto",
-        "33a19b60e25fb6f4435af53a3d42d493644827367e6453928554f43e49aa6f90")
-// expectedSignatureHex: "3045022100fd567d121db66e382991534ada77a6bd3106f0a1098c231e47993447cd6af2d002206b39cd0eb1bc8603e159ef5c20a5c8ad685a45b06ce9bebed3f153d10d93bed5"
+        verifyRFC6979(
+            "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364140",
+            "Satoshi Nakamoto",
+            "33a19b60e25fb6f4435af53a3d42d493644827367e6453928554f43e49aa6f90",
+            "3045022100fd567d121db66e382991534ada77a6bd3106f0a1098c231e47993447cd6af2d002206b39cd0eb1bc8603e159ef5c20a5c8ad685a45b06ce9bebed3f153d10d93bed5"
+        )
 
-        verifyRFC6979("f8b8af8ce3c7cca5e300d33939540c10d45ce001b8f252bfbc57ba0342904181",
-        "Alan Turing",
-        "525a82b70e67874398067543fd84c83d30c175fdc45fdeee082fe13b1d7cfdf1")
-//expectedSignatureHex: "304402207063ae83e7f62bbb171798131b4a0564b956930092b33b07b395615d9ec7e15c022058dfcc1e00a35e1572f366ffe34ba0fc47db1e7189759b9fb233c5b05ab388ea"
+        verifyRFC6979(
+            "f8b8af8ce3c7cca5e300d33939540c10d45ce001b8f252bfbc57ba0342904181",
+            "Alan Turing",
+            "525a82b70e67874398067543fd84c83d30c175fdc45fdeee082fe13b1d7cfdf1",
+            "304402207063ae83e7f62bbb171798131b4a0564b956930092b33b07b395615d9ec7e15c022058dfcc1e00a35e1572f366ffe34ba0fc47db1e7189759b9fb233c5b05ab388ea"
+        )
 
-        verifyRFC6979("0000000000000000000000000000000000000000000000000000000000000001",
-        "All those moments will be lost in time, like tears in rain. Time to die...",
-        "38aa22d72376b4dbc472e06c3ba403ee0a394da63fc58d88686c611aba98d6b3")
-// expectedSignatureHex: "30450221008600dbd41e348fe5c9465ab92d23e3db8b98b873beecd930736488696438cb6b0220547fe64427496db33bf66019dacbf0039c04199abb0122918601db38a72cfc21"
+        verifyRFC6979(
+            "0000000000000000000000000000000000000000000000000000000000000001",
+            "All those moments will be lost in time, like tears in rain. Time to die...",
+            "38aa22d72376b4dbc472e06c3ba403ee0a394da63fc58d88686c611aba98d6b3",
+            "30450221008600dbd41e348fe5c9465ab92d23e3db8b98b873beecd930736488696438cb6b0220547fe64427496db33bf66019dacbf0039c04199abb0122918601db38a72cfc21"
+        )
 
-        verifyRFC6979("e91671c46231f833a6406ccbea0e3e392c76c167bac1cb013f6f1013980455c2",
-        "There is a computer disease that anybody who works with computers knows about. It's a very serious disease and it interferes completely with the work. The trouble with computers is that you 'play' with them!",
-        "1f4b84c23a86a221d233f2521be018d9318639d5b8bbd6374a8a59232d16ad3d")
-//expectedSignatureHex: "3045022100b552edd27580141f3b2a5463048cb7cd3e047b97c9f98076c32dbdf85a68718b0220279fa72dd19bfae05577e06c7c0c1900c371fcd5893f7e1d56a37d30174671f6")
+        verifyRFC6979(
+            "e91671c46231f833a6406ccbea0e3e392c76c167bac1cb013f6f1013980455c2",
+            "There is a computer disease that anybody who works with computers knows about. It's a very serious disease and it interferes completely with the work. The trouble with computers is that you 'play' with them!",
+            "1f4b84c23a86a221d233f2521be018d9318639d5b8bbd6374a8a59232d16ad3d",
+            "3045022100b552edd27580141f3b2a5463048cb7cd3e047b97c9f98076c32dbdf85a68718b0220279fa72dd19bfae05577e06c7c0c1900c371fcd5893f7e1d56a37d30174671f6"
+        )
 
 //        verifyRFC6979("22a47fa09a223f2aa079edf85a7c2d4f8720ee63e502ee2869afab7de234b80c",
 //                      hash256(for: "test message"),
